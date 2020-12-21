a4615d67b27771a55801d99cacb3285d921c7b79
0SkillAllLuck <43417046+0SkillAllLuck@users.noreply.github.com>
2020-12-21 01:20:04 +0100

    Fix whitelist not working correctly

--- internal/server/common/whitelist.go
+++ internal/server/common/whitelist.go
@@ -1,50 +1,28 @@
 package common
 
 import (
+	"strings"
+
 	"github.com/qumine/qumine-server-java/internal/utils"
 	"github.com/sirupsen/logrus"
 )
 
-const whitelistPath = "whitelist.json"
-
-// User is a user on the whitelist
-type User struct {
-	// UUID is the uuid of the user
-	UUID string `json:"uuid"`
-	// Name is the name of the user
-	Name string `json:"name"`
-}
+const whitelistPath = "white-list.txt"
 
 // ConfigureWhitelist configures the whitelist.json
 func ConfigureWhitelist() error {
 	if !utils.GetEnvBool("SERVER_WHITE_LIST_OVERRIDE", false) && utils.FileExists(whitelistPath) {
-		logrus.Info("whitelist.json already exist, skipping configuration")
+		logrus.Infof("%s already exist, skipping configuration", whitelistPath)
 		return nil
 	}
 
-	whitelist := newWhitelist(utils.GetEnvStringList("SERVER_WHITE_LIST", ""))
+	whitelist := utils.GetEnvStringList("SERVER_WHITE_LIST", "")
 	logrus.WithField("whitelist", whitelist).Infof("%s not found, configuring it now", whitelistPath)
 
-	if err := utils.WriteFileAsJSON(whitelistPath, &whitelist); err != nil {
+	strings.Join(whitelist, "\n")
+	if err := utils.WriteFileAsString(whitelistPath, strings.Join(whitelist, "\n")); err != nil {
 		return err
 	}
 	logrus.Debugf("%s configured", whitelistPath)
 	return nil
-}
-
-func newWhitelist(users []string) []User {
-	var whitelist []User
-	for _, n := range users {
-		if n == "" {
-			continue
-		}
-		whitelist = append(whitelist, newUser(n))
-	}
-	return whitelist
-}
-
-func newUser(name string) User {
-	return User{
-		Name: name,
-	}
 }
