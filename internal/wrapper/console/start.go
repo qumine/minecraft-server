db4f86f68a4627d52c88533dd343fdea75bad576
0SkillAllLuck <43417046+0SkillAllLuck@users.noreply.github.com>
2020-12-22 15:38:43 +0100

    Fix stderr not showing

---
+++ internal/wrapper/console/start.go
@@ -0,0 +1,53 @@
+package console
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+)
+
+// Start will start the console, it will follow the logs
+func (c *Console) Start() {
+	go c.startStderr()
+	go c.startStdout()
+
+	for {
+	}
+}
+
+func (c *Console) startStderr() {
+	for {
+		if l, err := readLine(c.stderr); err == nil {
+			go func() {
+				for _, s := range c.subscriber {
+					s(l)
+				}
+			}()
+			fmt.Print(l)
+		}
+	}
+}
+
+func (c *Console) startStdout() {
+	for {
+		if l, err := readLine(c.stdout); err == nil {
+			go func() {
+				for _, s := range c.subscriber {
+					s(l)
+				}
+			}()
+			fmt.Print(l)
+		}
+	}
+}
+
+func readLine(reader *bufio.Reader) (string, error) {
+	if reader == nil {
+		return "", io.EOF
+	}
+	l, err := reader.ReadString('\n')
+	if err == io.EOF {
+		return "", io.EOF
+	}
+	return l, nil
+}
